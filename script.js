378c899083408bf1c1c00254cdc1e6aefbd7a380
Douglasso10 <84336458+Douglasso10@users.noreply.github.com>
2022-09-02 20:49:37 -0300

    Adicionando as tarefas via Js e as estilizando

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const input = document.querySelector('.input')
-const btn = document.querySelector('.btn')
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const tarefa = document.querySelector('.tarefa');
+
 
 const validaInput = () =>  input.value.trim().length > 0;
 
@@ -7,8 +9,26 @@ const adicionarTarefa = () => {
     const inputValido = validaInput();
 
     if(!inputValido){
-        return input.classList.add('error');
+        return input.classList.add("error");
     }
+
+    const criarTarefa = document.createElement("div");
+    criarTarefa.classList.add("novaTarefa");
+
+    const paragrafo = document.createElement("p");
+    paragrafo.innerText = input.value;
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("far");
+    deleteIcon.classList.add("fa-trash-alt");
+
+    criarTarefa.appendChild(paragrafo);
+    criarTarefa.appendChild(deleteIcon);
+
+    tarefa.appendChild(criarTarefa);
+
+    input.value = "";
+    
 }
 
 const mudarInput = () =>{
